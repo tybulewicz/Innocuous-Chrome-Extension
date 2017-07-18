1468a371866e05ca00f5f49b34d398ef6aff7164
Tomasz Tybulewicz <tomasz@tybulewicz.com>
2017-07-18 07:39:56 +0200

    Spoofing Twitter
    https://elaineou.com/2017/01/19/how-the-twitter-app-bypasses-paywalls/

--- background.js
+++ background.js
@@ -1,31 +1,33 @@
-var ALLOW_COOKIES = ["nytimes", "ft.com"]
+var VIA_TWITTER = ["wsj.com"]
 
 function changeRefer(details) {
+
   foundReferer = false;
-  foundUA = false
+  foundUA = false;
+
+  var useTwitter = VIA_TWITTER.map(function(url) {
+    if (details.url.includes(url)) {
+      return true;
+    }
+    return false;
+  })
+  .reduce(function(a, b) { return a || b}, false);
 
   var reqHeaders = details.requestHeaders.filter(function(header) {
+
     // block cookies by default
     if (header.name !== "Cookie") {
       return header;
     } 
 
-    allowHeader = ALLOW_COOKIES.map(function(url) {
-      if (details.url.includes(url)) {
-        return true;
-      }
-      return false;
-    });
-    if (allowHeader.reduce(function(a, b) { return a || b}, false)) { return header; }
-
   }).map(function(header) {
     
     if (header.name === "Referer") {
-      header.value = "https://www.google.com/";
+      header.value = setRefer(useTwitter);
       foundReferer = true;
     }
     if (header.name === "User-Agent") {
-      header.value = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)";
+      header.value = setUserAgent(useTwitter);
       foundUA = true;
     }
     return header;
@@ -35,16 +37,15 @@ function changeRefer(details) {
   if (!foundReferer) {
     reqHeaders.push({
       "name": "Referer",
-      "value": "https://www.google.com/"
+      "value": setRefer(useTwitter)
     })
   }
   if (!foundUA) {
     reqHeaders.push({
       "name": "User-Agent",
-      "value": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+      "value": setUserAgent(useTwitter)
     })
   }
-  console.log(reqHeaders);
   return {requestHeaders: reqHeaders};
 }
 
@@ -55,6 +56,16 @@ function blockCookies(details) {
     }
   }
   return {responseHeaders: details.responseHeaders};
+}
+
+function setRefer(useTwitter) {
+  if (useTwitter) return "https://t.co/T1323aaaa"; 
+  else return "https://www.google.com/";
+}
+
+function setUserAgent(useTwitter) {
+  if (useTwitter) return "Mozilla/5.0 (iPhone; CPU iPhone OS 10_2 like Mac OS X) AppleWebKit/602.1.32 (KHTML, like Gecko) Mobile/14C92 Twitter for iPhone";
+  else return "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)";
 }
 
 chrome.webRequest.onBeforeSendHeaders.addListener(changeRefer, {
